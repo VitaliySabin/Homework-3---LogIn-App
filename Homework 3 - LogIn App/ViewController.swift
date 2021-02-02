1db039403efb93882a253da40cb83e57dcab8c27
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-02-02 18:10:55 +0300

    Сделал аллерты для кнопок "Забыл пароль" и "Забыл логин"

--- Homework 3 - LogIn App/ViewController.swift
+++ Homework 3 - LogIn App/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+//    @IBAction func forgotUsernameAction() {
+//        showAlert(with: "Wrong username!", and: "Alexey, your username is Alexey!")
+//    }
+//
+//    @IBAction func forgotPasswordAction() {
+//        showAlert(with: "Wrong password!", and: "Alexey, your password is discoman!")
+//    }
+//}
+//extension ViewController {
+//    private func showAlert(with title: String, and message: String) {
+//        let alert = UIAlertController(title: title,
+//                                      message: message,
+//                                      preferredStyle: .alert)
+//        let okAction = UIAlertAction(title: "OK", style: .default)
+//        alert.addAction(okAction)
+//        present(alert, animated: true)
+//    }
 }
-
---
+++ Homework 3 - LogIn App/WelcomeUserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeUserViewController.swift
+//  Homework 3 - LogIn App
+//
+//  Created by Виталий Сабин on 02.02.2021.
+//
+
+import UIKit
+
+class WelcomeUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
