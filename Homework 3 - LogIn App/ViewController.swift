a98cfaf316b59a79feeee5dc297547106a74169e
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2021-02-06 14:41:40 +0300

    Исправил расстановку элементов

--- Homework 3 - LogIn App/ViewController.swift
+++ Homework 3 - LogIn App/ViewController.swift
@@ -11,24 +11,5 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
-//    @IBAction func forgotUsernameAction() {
-//        showAlert(with: "Wrong username!", and: "Alexey, your username is Alexey!")
-//    }
-//
-//    @IBAction func forgotPasswordAction() {
-//        showAlert(with: "Wrong password!", and: "Alexey, your password is discoman!")
-//    }
-//}
-//extension ViewController {
-//    private func showAlert(with title: String, and message: String) {
-//        let alert = UIAlertController(title: title,
-//                                      message: message,
-//                                      preferredStyle: .alert)
-//        let okAction = UIAlertAction(title: "OK", style: .default)
-//        alert.addAction(okAction)
-//        present(alert, animated: true)
-//    }
 }
